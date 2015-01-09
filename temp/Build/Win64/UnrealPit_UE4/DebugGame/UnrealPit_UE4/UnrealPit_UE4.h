d9b3c6148abfebd498a8fbeff2fddf90e42ed706
Thomas Krause <thomas.krause@gmail.com>
2015-01-09 22:56:29 +0100

    deletion

---
+++ temp/Build/Win64/UnrealPit_UE4/DebugGame/UnrealPit_UE4/UnrealPit_UE4.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UNREALPIT_UE4_H__
+#define __AUTO_UNREALPIT_UE4_H__
+//Last Write: 24.11.2014 10:33:36 +00:00
+#include "D:\Eigene Dateien\Dropbox\Softwareentwicklung\Unreal Projects\UnrealPit_UE4\Source\UnrealPit_UE4\Public\UnrealPit_UE4.h"
+#endif//__AUTO_UNREALPIT_UE4_H__
---
+++ temp/Build/Win64/UnrealPit_UE4/Development/UnrealPit_UE4/UnrealPit_UE4.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UNREALPIT_UE4_H__
+#define __AUTO_UNREALPIT_UE4_H__
+//Last Write: 30.11.2014 16:08:12 +00:00
+#include "D:\Eigene Dateien\Dropbox\Softwareentwicklung\Unreal Projects\UnrealPit_UE4\Source\UnrealPit_UE4\Public\UnrealPit_UE4.h"
+#endif//__AUTO_UNREALPIT_UE4_H__
