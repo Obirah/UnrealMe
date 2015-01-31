4a80e33ef802fa6b52c41fb001779a7fb9d09f4d
Thomas Krause <thomas.krause@gmail.com>
2015-01-31 17:49:56 +0100

    Player controller adjustments

--- Source/ThirdParty/K4WLib/K4WLib.Build.cs
+++ Source/ThirdParty/K4WLib/K4WLib.Build.cs
@@ -1,5 +1,4 @@
 ﻿// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 
 public class K4WLib : ModuleRules
@@ -7,26 +6,31 @@ public class K4WLib : ModuleRules
     public K4WLib(TargetInfo Target)
 	{
 		Type = ModuleType.External;
-
+		
+		/* Path to the Kinect V2 SDK read from environment variable */
         string SDKDIR = Utils.ResolveEnvironmentVariable("%KINECTSDK20_DIR%");
-
+		/* Change path format from Windows backslashes to forward slashes */
         SDKDIR = SDKDIR.Replace("\\", "/");
 
 		if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
 		{
+			/* Add the SDK's include folder to the Unreal include paths. */
             PublicIncludePaths.Add(SDKDIR+"inc/");
-
+			
+			/* Use the fitting plattform suffix */
             string PlatformPath =  (Target.Platform == UnrealTargetPlatform.Win64) ? "x64/" : "x86/";
-
+			/* Path to the SDK's lib folder*/
             string LibPath = SDKDIR+"Lib/"+PlatformPath;
-
+			
+			/* Add the Kinect library path to Unreal's libarary paths */
             PublicLibraryPaths.Add(LibPath);
+			/* Add the needed static library */
             PublicAdditionalLibraries.Add("Kinect20.lib");
 
+			/* Path to the SDK's redistributable directory */
             string redistPath = SDKDIR + "Redist/";
-
+			/* Add the VisualGestureBuilder dynamic library to Unreal's DLLs to be loaded. */
             PublicDelayLoadDLLs.AddRange(new string[] { redistPath+"VGB/"+PlatformPath+"Kinect20.VisualGestureBuilder.dll" });
-
 		}
 	}
 }
